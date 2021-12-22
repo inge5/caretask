337ca6668600ac79fdcdc7fb842aa223d05bfccc
Nelson Zambrano <inge5neuromedia@gmail.com>
2021-12-22 16:16:36 -0500

    commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnimateContactoComponent } from './animate-contacto/animate-contacto.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CaretaskComponent, SafeHtmlPipe } from './caretask/caretask.component';
+import { SidebarComponent } from './caretask/sidebar/sidebar.component';
+import { SubmenuComponent } from './caretask/submenu/submenu.component';
+import { SidebarContactComponent } from './sidebar-contact/sidebar-contact.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { SomosComponent } from './somos/somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimateContactoComponent,
+    HeaderComponent,
+    FooterComponent,
+    CaretaskComponent,
+    SidebarComponent,
+    SubmenuComponent,
+    SidebarContactComponent,
+    SafeHtmlPipe,
+    SomosComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CarouselModule,
+    NgxSkeletonLoaderModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
